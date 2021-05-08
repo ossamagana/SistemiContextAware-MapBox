31671a6ea7bec88443b31f217a5698936ca579df
ossamagana <ossama.gana@studio.unibo.it>
2021-05-08 15:09:48 +0200

    Add cluster and marker elements

---
+++ src/config.js
@@ -0,0 +1,49 @@
+export const color = {
+    primary: "#E83D56",
+    primary_lighter: "#EA4961",
+    primary_light: "#ED6579",
+    primary_ulight: "#EFF8F7",
+    accent: "#52B3A1",
+    gunmetal: "#224552",
+    gunmetal_light: "#C4DDE6",
+    text_light: "#727272",
+    warning900: "#EF8511",
+    black: "#000000",
+    white: "#FFFFFF",
+    green: "#23825F",
+    gray_light: "#E9E9E9",
+    gray_lighter: "#F5F5F5",
+    error: "#ff0033",
+    white_blur: "rgba(250, 250, 250, 0.5)",
+    primary_blur: "rgba(232,61,86, 0.5)",
+    accent_blur: "rgba(82,179,161, 0.5)",
+    gunmetal_blur: "rgba(34,69,82, 0.5)",
+}
+
+export const theme = {
+    colors: color,
+    fontFamily: {
+        primary: "Poppins, sans-serif",
+    },
+    shadows: {
+        "up-3": "0px -1px 1px -1px rgba(8, 11, 14, 0.06), 0 -6px 6px -1px rgba(8, 11, 14, 0.1)",
+        banner_button: "rgba(7, 12, 14, 0.06) 0px 0px 1px 0px, rgba(8, 10, 14, 0.39) 0px 0px 16px -1px",
+        "top-3":
+            "rgba(8, 11, 14, 0.06) 0px -1px 1px -1px, rgba(8, 11, 14, 0.1) 0px -4px 6px -1px, rgba(8, 11, 14, 0.1) 0px 4px 6px -1px",
+        primary: " 0px 3px 20px 0px rgba(232,61,86, 0.55)",
+        accent: " 0px 3px 20px 0px rgba(82,179,161,0.15)",
+        gunmetal: " 0px 3px 20px 0px rgba(34,69,82,0.15)",
+    },
+    rounded: {
+        upOnly: "12px 12px 0 0",
+        "b-10px": "0 0 10px 10px",
+        "t-20px": "20px 20px 0 0",
+        "pill-20px": "0 20px 0 20px",
+        "pillAlt-20px": "20px 0 40px 20px",
+        "20px": "20px",
+        "15px": "15px",
+    },
+    transition: {
+        fast: "all 0.2s ease-in-out",
+    },
+}
